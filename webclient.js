453a9ec48edc61ad06b88dbc758554b4833d87fb
Dave Jacobowitz <david.jacobowitz@gmail.com>
2017-04-11 16:13:26 +0000

    better palette and more tiles

--- webclient.js
+++ webclient.js
@@ -1,7 +1,7 @@
 
 config = {
- x_tiles: 5,
- y_tiles: 5,
+ x_tiles: 10,
+ y_tiles: 10,
 
 };
 
@@ -20,6 +20,43 @@ var params = {
     y_max: 2,
 };
 
+/**
+ * Converts an HSL color value to RGB. Conversion formula
+ * adapted from http://en.wikipedia.org/wiki/HSL_color_space.
+ * Assumes h, s, and l are contained in the set [0, 1] and
+ * returns r, g, and b in the set [0, 255].
+ *
+ * @param   {number}  h       The hue
+ * @param   {number}  s       The saturation
+ * @param   {number}  l       The lightness
+ * @return  {Array}           The RGB representation
+ */
+/* cribbed from http://stackoverflow.com/questions/2353211/hsl-to-rgb-color-conversion */
+function hslToRgb(h, s, l){
+    var r, g, b;
+
+    if(s == 0){
+        r = g = b = l; // achromatic
+    }else{
+        var hue2rgb = function hue2rgb(p, q, t){
+            if(t < 0) t += 1;
+            if(t > 1) t -= 1;
+            if(t < 1/6) return p + (q - p) * 6 * t;
+            if(t < 1/2) return q;
+            if(t < 2/3) return p + (q - p) * (2/3 - t) * 6;
+            return p;
+        }
+
+        var q = l < 0.5 ? l * (1 + s) : l + s - l * s;
+        var p = 2 * l - q;
+        r = hue2rgb(p, q, h + 1/3);
+        g = hue2rgb(p, q, h);
+        b = hue2rgb(p, q, h - 1/3);
+    }
+
+    return [Math.round(r * 255), Math.round(g * 255), Math.round(b * 255)];
+}
+
 function handleReturnedData(res) {
     console.log(res.params);
 
@@ -35,14 +72,19 @@ function handleReturnedData(res) {
 
     for (var i=0; i< res.data.length; i++) {
         var dv = res.data[i];
-        if (true) {
+        if (false) {
             iData.data[4*i + 0] = dv < 128 ? 2 * dv : 255;
             iData.data[4*i + 1] = dv < 64  ? 4 * dv : 255;
             iData.data[4*i + 2] = dv < 84  ? 3 * dv : 255;
-        } else {
+        } else if (false){
             iData.data[4*i + 0] = Math.sqrt(dv) * 16.0;
             iData.data[4*i + 1] = 0;
             iData.data[4*i + 2] = dv * dv / 256;
+        } else {
+            var color = hslToRgb(dv/255.0, 0.5, 0.5);
+            iData.data[4*i + 0] = color[0];
+            iData.data[4*i + 1] = color[1];
+            iData.data[4*i + 2] = color[2];
         }
         iData.data[4*i + 3] = 0xff;
     }
